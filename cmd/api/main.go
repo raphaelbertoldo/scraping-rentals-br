92d1143ce4316021afa51a17d76a93c19f9186f9
Raphael Ferreira Bertoldo <rxphxel7k@gmail.com>
2024-09-30 19:04:30 -0300

    ✨

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/raphaelbertoldo/scraping-rentals-br/internal/ivan/search/ivanService"
+	"github.com/raphaelbertoldo/scraping-rentals-br/internal/ivan/ivanService"
 )
 
 func main() {
