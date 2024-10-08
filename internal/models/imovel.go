f2a3b19a87466dd8c2b24b2e6895f3fe2e4b00e8
Raphael Ferreira Bertoldo <rxphxel7k@gmail.com>
2024-10-08 01:07:28 -0300

    improve model

--- internal/models/imovel.go
+++ internal/models/imovel.go
@@ -4,8 +4,9 @@ type Imovel struct {
 	Url      string   `json:"url"`
 	Title    string   `json:"title"`
 	Type     string   `json:"type"`
-	Subtitle string   `json:"subtitle"`
+	Subtitle string   `json:"subtitle"` // describ
 	Info     string   `json:"info"`
+	Address  string   `json:"address"`
 	Price    string   `json:"price"`
 	Imgs     []string `json:"imgs"`
 }
